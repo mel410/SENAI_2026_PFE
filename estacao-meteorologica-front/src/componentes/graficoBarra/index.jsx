dacea5a9aafc1a7fc0ddbd02157268f3ac306bb5
melzinha <melissa.b.barbosa@aluno.senai.br>
2026-04-30 10:45:41 -0300

    commit mel cara de pastel

--- estacao-meteorologica-front/src/componentes/graficoBarra/index.jsx
+++ estacao-meteorologica-front/src/componentes/graficoBarra/index.jsx
@@ -1,9 +1,41 @@
 import {Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend} from 'chart.js'
 import {Bar} from 'react-chartjs-2'
+import {faker} from '@faker-js/faker'
+
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
 
 export default function GraficoBarra(){
+    const opcoes = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Medidas de temperaturas por mês',
+            }
+        },
+    }   
+
+    const labels = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+
+    const dados = {
+        labels,
+        datasets: [
+            {
+                label: 'Chuva (mm)',
+                data: labels.map(() => faker.number.int({min: 0, max: 500})),
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            },
+            {
+                label: 'Temperatura (°C)',
+                data: labels.map(() => faker.number.int({min: -10, max: 40})),
+                backgroundColor: 'rgba(53, 162, 235, 0.5)',
+            },
+        ]
+    }
     return(
-        <></>
+        <Bar options={opcoes} data={dados}/>
     )
 }

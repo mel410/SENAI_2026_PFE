7dae3f8c15e10305e0a352735c0bb0aab9cf9290
melzinha <melissa.b.barbosa@aluno.senai.br>
2026-08-20 10:09:25 -0300

    aula2008

---
+++ 2 semestre/aula2008-juros-simples/app/page.js
@@ -0,0 +1,11 @@
+'use client';
+
+import JurosSimples from "./jurossimples/page";
+
+export default function Home() {
+  return (
+    <main className="pagina">
+      <JurosSimples />
+    </main>
+  );
+}
